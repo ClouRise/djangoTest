9b9784e55129d3622d817215458a27accb984134
Boris <mr.clourise@gmail.com>
2024-10-13 19:17:46 +0300

    1

--- djangotest/main/views.py
+++ djangotest/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("<h4>main</h4>")
+    return render(request, 'main/index.html')
 def secondPage(request):
     return HttpResponse("<h4>second page</h4>")
